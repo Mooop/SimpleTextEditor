d76e422a5054cc6f81d6fbc109107cbf5e55e842
Mop <mop737339715@gmail.com>
2018-02-07 15:22:55 +0800

    init

--- app/src/main/java/sample/mop/com/simpletexteditor/MainActivity.java
+++ app/src/main/java/sample/mop/com/simpletexteditor/MainActivity.java
@@ -1,7 +1,13 @@
 package sample.mop.com.simpletexteditor;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.mop.simple.texteditor.SimpleTextEditorInit;
+import com.mop.simple.texteditor.view.DraftBoxActivity;
+import com.mop.simple.texteditor.view.SimpleTextViewActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SimpleTextEditorInit.init(this);
+        SimpleTextEditorInit.setLogEnable(true);
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), SimpleTextViewActivity.class));
+            }
+        });
+
+        findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), DraftBoxActivity.class));
+            }
+        });
     }
 }
